d054f58a814a0c99e378a068849d000c08a783a7
Raymond Schouten <raymondschouten@users.noreply.github.com>
2026-07-21 16:24:31 +0200

    #97: Implemented enhanced event for postRemove()

---
+++ doctrine-enhanced-events/src/PostRemoveEventArgs.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Copyright (c) 2026-present DarkWeb Design.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace DarkWebDesign\DoctrineEnhancedEvents;
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\ORMInvalidArgumentException;
+use Doctrine\Persistence\ObjectManager;
+
+/**
+ * @author Raymond Schouten
+ *
+ * @since 2.9
+ */
+class PostRemoveEventArgs extends LifecycleEventArgs
+{
+    /** @var array<string, mixed> */
+    private $deletedIdentifierValues;
+
+    /**
+     * @param array<string, mixed> $deletedIdentifierValues
+     */
+    public function __construct(object $object, array $deletedIdentifierValues, ObjectManager $objectManager)
+    {
+        parent::__construct($object, $objectManager);
+
+        $this->deletedIdentifierValues = $deletedIdentifierValues;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function getDeletedIdentifierValues(): array
+    {
+        return $this->deletedIdentifierValues;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDeletedSingleIdentifierValue()
+    {
+        if (count($this->deletedIdentifierValues) > 1) {
+            throw ORMInvalidArgumentException::invalidCompositeIdentifier();
+        }
+
+        return reset($this->deletedIdentifierValues);
+    }
+}
